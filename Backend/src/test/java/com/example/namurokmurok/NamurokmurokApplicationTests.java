f6777111476c7557483dc99e890ded133750aee8
Juri-Jang <jjr84@dgu.ac.kr>
2025-10-13 19:09:12 +0900

    [BE] init : 프로젝트 초기 세팅

---
+++ Backend/src/test/java/com/example/namurokmurok/NamurokmurokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.namurokmurok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamurokmurokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
