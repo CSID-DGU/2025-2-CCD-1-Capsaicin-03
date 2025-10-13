9458e4ca526aeea647d2ecda264428f0668b55dc
Juri-Jang <jjr84@dgu.ac.kr>
2025-10-13 19:09:12 +0900

    [BE] init : 프로젝트 초기 세팅

---
+++ Backend/src/main/java/com/example/namurokmurok/NamurokmurokApplication.java
@@ -0,0 +1,13 @@
+package com.example.namurokmurok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamurokmurokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamurokmurokApplication.class, args);
+    }
+
+}
