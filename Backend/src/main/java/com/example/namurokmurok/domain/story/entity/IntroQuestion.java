4424334379004dc145e5977f8aea85ff0f323f26
Juri-Jang <jjr84@dgu.ac.kr>
2025-11-03 21:27:19 +0900

    [BE] Feat : IntroQuestion 엔티티 생성

---
+++ Backend/src/main/java/com/example/namurokmurok/domain/story/entity/IntroQuestion.java
@@ -0,0 +1,33 @@
+package com.example.namurokmurok.domain.story.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@Table(name = "intro_questions")
+public class IntroQuestion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "text_content" ,nullable = false)
+    private String textContent;
+
+    @Column(name = "audio_url" ,nullable = false)
+    private String audioUrl;
+
+    @Column(name = "img_url" ,nullable = false)
+    private String imgUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "story_id", nullable = false)
+    private Story story;
+}
