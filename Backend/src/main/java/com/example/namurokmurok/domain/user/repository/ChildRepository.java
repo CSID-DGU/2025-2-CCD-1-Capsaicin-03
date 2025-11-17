afac0ffe766cadb65cd03788d1be1bda95bdd791
Juri-Jang <jjr84@dgu.ac.kr>
2025-11-17 21:45:03 +0900

    [BE] Feat : 세션id 기반 대화 컬럼 생성 로직 구현

--- Backend/src/main/java/com/example/namurokmurok/domain/user/repository/ChildRepository.java
+++ Backend/src/main/java/com/example/namurokmurok/domain/user/repository/ChildRepository.java
@@ -4,7 +4,10 @@ import com.example.namurokmurok.domain.user.entity.Child;
 import com.example.namurokmurok.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface ChildRepository extends JpaRepository<Child, Long> {
     boolean existsByUser(User user);
+    Optional<Child> findByName(String name);
 }
