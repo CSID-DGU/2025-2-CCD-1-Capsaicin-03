e1ee9c6cada60018ce733418d522db125f8524a9
Juri-Jang <jjr84@dgu.ac.kr>
2025-11-18 20:59:22 +0900

    [AI] Fix : redis username 변수 추가

--- AI/app/core/config.py
+++ AI/app/core/config.py
@@ -24,6 +24,7 @@ class Settings(BaseSettings):
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
     REDIS_DB: int = 0
+    REDIS_USERNAME: str | None = None
     REDIS_PASSWORD: Optional[str] = None
     REDIS_URL: Optional[str] = None
     
